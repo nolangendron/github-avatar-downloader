51145c34da4ba24da81ad7261c200e0f37f70fd2
nolangendron <nolanglengendron@gmail.com>
2019-06-03 21:07:56 +0000

    Defined getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("https://github.com/nodejs/node", "node", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
