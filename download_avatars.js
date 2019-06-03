b8baf53c252c017f43c498db88f5c054687a89f0
nolangendron <nolanglengendron@gmail.com>
2019-06-03 21:57:49 +0000

    add an authorization to header

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,24 @@
 var request = require('request');
+var github_token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    headers: {
+      'User-Agent': 'nolangendron',
+      'Authorization': token github_token
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  })
 
 }
 
-getRepoContributors("https://github.com/nodejs/node", "node", function(err, result) {
+getRepoContributors("https://api.github.com/repos/jquery/jquery/contributors", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
